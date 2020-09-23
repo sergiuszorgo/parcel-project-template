d4850860c116f63fc0558fec417921a1afa8e533
sergiuszorgo <sergiuszorgo@users.noreply.github.com>
2020-09-23 10:53:58 +0300

    Обновлён скрипт

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    navItems: document.querySelectorAll('[data-modal] .menu-item'),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.navItems.forEach(item => item.addEventListener('click', toggleModal));
   function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
